03949fabe405bcc31f9bb9c3031fde69f6524fa5
EmilyYoung71415 <775363160@qq.com>
2020-01-28 23:56:54 +0800

    顺序表

---
+++ 0-数组/1-数组反转.js
@@ -0,0 +1,34 @@
+/***
+ * 将数组中所有元素逆置，空间复杂度为O(1)
+ * 不得使用api
+ */
+
+/*****
+ * 思路：
+ * 使用两个指针，分别从两头出发往数组中间走，互相交换，直至指针相遇
+ * 优化：变量j=len-i-1，扫描数组的前半部分，其对称元素data[len-i-1]
+ */
+reverse([1,2,3,4,5,6,7,8])
+function reverse1(arr) {
+    let i=0;
+    let j=arr.length-1;
+
+    while(i<=j) {
+        // arr[i] arr[j] 数组交换
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+    console.log(arr);
+};
+
+function reverse(arr) {
+    for(let i=0;i<Math.floor(arr.length/2);i++) {
+        let temp = arr[i];
+        arr[i] =  arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    console.log(arr);
+}
---
+++ 0-数组/1-有序数组去重.js
@@ -0,0 +1,26 @@
+/****
+ * 思路：
+ * 遍历访问的时候，如果后面一个元素不等于前面的元素，将该非重复元素移动到最新xx位不含重复元素的有序数组里
+ * 如果是等于前面的元素，则不管，直至遍历到末尾，
+ * 然后修改原数组的长度为最新不含重复元素的有序数组的长度 (arr.length = x) 相当于只保留前x-1个元素
+ * 
+ * 即插入排序的思想
+ * 需要两个指针，i指向不含重复元素的数组末尾，j为原数组遍历元素
+ *                                                                                                                            
+ */
+unionSet([1,1,2]);
+function unionSet(arr) {
+    if (!arr || !arr.length) return;
+
+    for (let i=0,j=1,len=arr.length;j<len;j++) {
+        if (arr[j] !== arr[i]) {
+            arr[++i] = arr[j];
+        }
+        if (j===len-1) {
+            arr.length = i+1;
+        }
+    }
+    console.log(arr);
+}
+
+// 扩展： 如果是无序数组，时间复杂度为O(N)要求，求解决方案？ ==> hash表
