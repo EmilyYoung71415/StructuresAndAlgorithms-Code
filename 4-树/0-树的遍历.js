6d9966f1b2805a149ec10402cd1a714316821e0d
EmilyYoung71415 <775363160@qq.com>
2020-01-29 23:58:38 +0800

    day77-链表 & 树 遍历框架

---
+++ 4-树/0-树的遍历.js
@@ -0,0 +1,25 @@
+/***
+ * 二叉树的遍历和链表的遍历，相似
+ * 二叉树的结构与链表结构，相似
+ * 由此推得，多叉树的遍历？
+ */
+
+/* 基本的二叉树结点 */
+function treeNode(data) {
+    this.data = data;
+    this.left = this.right = null;
+}
+
+function traverse(root) {
+    if (root==null) return;
+    // 访问root.data
+    traverse(root.left);
+    traverse(root.right);
+}
+
+// 多叉树遍历
+function traverse(root) {
+    for(let child of root.children) {
+        traverse(child);
+    }
+}
