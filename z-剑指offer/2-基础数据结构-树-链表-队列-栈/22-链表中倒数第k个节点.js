708649188943ed9db785dd069521c9939fcb3719
EmilyYoung71415 <775363160@qq.com>
2020-02-24 23:27:22 +0800

    day101-链表经典题

---
+++ z-剑指offer/2-基础数据结构-树-链表-队列-栈/22-链表中倒数第k个节点.js
@@ -0,0 +1,29 @@
+/***
+ * https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数
+ * 
+ */
+
+// 给定一个链表: 1->2->3->4->5, 和 k = 2.
+// 返回链表 4->5.
+
+
+/****
+ * 思路：
+ *      快慢指针
+ *      快指针先走k步，然后快慢指针齐头并进，等快指针到达链尾时，慢指针所指即为所求
+ */
+function getKthFromEnd(head, k) {
+    let fast = slow = head;
+
+    while (k--) {
+        fast = fast.next;
+    }
+
+    while (fast) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;
+}
