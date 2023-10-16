e5bb3efd1b47ed842bbd93a2e015d8d4aecbb2da
EmilyYoung71415 <775363160@qq.com>
2023-10-17 05:38:53 +0800

    refactor: 1857 largestPathValue dfs

--- Mastery/practice/1857. Largest Color Value in a Directed Graph/index.test.ts
+++ Mastery/practice/1857. Largest Color Value in a Directed Graph/index.test.ts
@@ -1,4 +1,3 @@
-// import { largestPathValue } from './index';
 import { largestPathValue as largestPathValue_bfs } from './bfs';
 import { largestPathValue as largestPathValue_dfs } from './post_dfs';
 
@@ -13,25 +12,25 @@ const testData = [
     ],
     expected: 3,
   },
-  // {
-  //   colors: 'nnllnzznn',
-  //   edges: [
-  //     [0, 1],
-  //     [1, 2],
-  //     [2, 3],
-  //     [2, 4],
-  //     [3, 5],
-  //     [4, 6],
-  //     [3, 6],
-  //     [5, 6],
-  //     [6, 7],
-  //     [7, 8],
-  //   ],
-  //   expected: 5,
-  // },
+  {
+    colors: 'nnllnzznn',
+    edges: [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [2, 4],
+      [3, 5],
+      [4, 6],
+      [3, 6],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+    ],
+    expected: 5,
+  },
 ];
 
-test('largestPathValue_bfs should work', () => {
+test.skip('largestPathValue_bfs should work', () => {
   testData.forEach(test => {
     const { colors, edges, expected } = test;
     const output = largestPathValue_bfs(colors, edges);
@@ -39,7 +38,7 @@ test('largestPathValue_bfs should work', () => {
   });
 });
 
-test.skip('largestPathValue_dfs should work', () => {
+test('largestPathValue_dfs should work', () => {
   testData.forEach(test => {
     const { colors, edges, expected } = test;
     const output = largestPathValue_dfs(colors, edges);
