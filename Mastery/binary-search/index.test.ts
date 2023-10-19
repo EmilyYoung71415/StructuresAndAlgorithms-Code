b67d6fe012445fee1321078fbc2f40ba24e52395
EmilyYoung71415 <775363160@qq.com>
2023-10-20 05:09:45 +0800

    feat: binary search boundary

--- Mastery/binary-search/index.test.ts
+++ Mastery/binary-search/index.test.ts
@@ -2,6 +2,7 @@ import { binarySearch as binarySearch_for } from './preorder-for';
 import { binarySearch as binarySearch_bfs } from './preorder-bfs';
 import { binarySearch as binarySearch_dfs } from './preorder-dfs';
 import { binarySearch as binarySearch_post_dfs } from './postorder-dfs';
+import { binarySearch_left, binarySearch_right } from './bounder-left-right';
 
 const tests: Array<[[number[], number], number]> = [[[[1, 2, 8, 4], 8], 2]];
 
@@ -34,5 +35,19 @@ test('binarySearch_post_dfs should work', () => {
     const [input, expected] = test;
     const output = binarySearch_post_dfs(...input);
     expect(output).toBe(expected);
+  });
+});
+
+describe('binary search boundary should work', () => {
+  const input = [1, 2, 3, 3, 3, 4, 5];
+  const target = 3;
+  test('left boundary', () => {
+    const output = binarySearch_left(input, target);
+    expect(output).toBe(2);
+  });
+
+  test('right boundary', () => {
+    const output = binarySearch_right(input, target);
+    expect(output).toBe(4);
   });
 });
