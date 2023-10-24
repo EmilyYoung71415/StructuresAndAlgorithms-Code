7d36322a969f6b1240291893572799ff07ca2446
EmilyYoung71415 <775363160@qq.com>
2023-10-24 12:39:31 +0800

    refactor: quick sort three color

--- Mastery/practice/215.findKthLargest/half.ts
+++ Mastery/practice/215.findKthLargest/half.ts
@@ -1,41 +1,61 @@
 // 折半优化： 用于求top k问题
 import { swap } from '@utils';
 
-export function findKthLargest(nums: number[], k: number) {
-  //将[L,R]按照枢轴分为三份  三色问题 → > pivot | = pivot | < pivot
-  const partition = (L, R) => {
-    //1、根据枢轴将数组中的数按照大小分成左右两半，并且中间为pivot
-    const pivotIndex = L + Math.floor(Math.random() * (R - L + 1));
-    const pivot = nums[pivotIndex];
-    let p0 = L; //左边 = ~0
-    let p1 = L; //左边 = ~0~1
-    for (let i = L; i <= R; ++i) {
-      //012[1]
-      if (nums[i] == pivot) {
-        swap(nums, i, p1);
-        ++p1;
-      } else if (nums[i] > pivot) {
-        //case1:012[0]
-        swap(nums, i, p0);
-        //case2:002[0]
-        if (p0 < p1) swap(nums, i, p1);
-        ++p0;
-        ++p1;
-      }
+//将[L,R]按照枢轴分为三份  三色问题 → > pivot | = pivot | < pivot
+// < pivot | = pivot | > pivot
+// 将数组分成 [ <left, (left, right) , >right]
+const partition = (nums: number[], left: number, right: number) => {
+  const pivotIndex = left + Math.floor(Math.random() * (right - left + 1));
+  const p = nums[pivotIndex];
+
+  let l = left;
+  let r = right;
+  let i = l;
+
+  // 求k大的， return nums[k-1]; nums降序
+  while (i <= r) {
+    if (nums[i] < p) {
+      swap(nums, i, r--);
+    } else if (nums[i] > p) {
+      swap(nums, i++, l++);
+    } else {
+      i++;
     }
-    return [p0, p1 - 1];
-  };
+  }
 
-  const dfs = (L, R) => {
-    //边界状态
-    if (R - L <= 0) return;
-    const [l, r] = partition(L, R);
+  return [l, r];
+};
 
-    //折半搜索：[L,l-1] → [l,r] → [r+1,R]
-    if (k <= l) {
-      dfs(L, l - 1);
-    } else if (k > r + 1) {
-      dfs(r + 1, R);
+export function findKthLargest2(nums: number[], k: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const [l, r] = partition(nums, left, right);
+
+    if (k - 1 >= l && k - 1 <= r) {
+      return nums[k - 1];
+    } else if (k - 1 < l) {
+      right = l - 1;
+    } else {
+      left = r + 1;
+    }
+  }
+
+  return nums[left];
+}
+
+export function findKthLargest_dfs(nums: number[], k: number): number {
+  const dfs = (left: number, right: number): void => {
+    if (left > right) return;
+
+    const [l, r] = partition(nums, left, right);
+
+    if (k - 1 >= l && k - 1 <= r) return;
+    if (k - 1 < l) {
+      dfs(left, l - 1);
+    } else {
+      dfs(r + 1, right);
     }
   };
 
