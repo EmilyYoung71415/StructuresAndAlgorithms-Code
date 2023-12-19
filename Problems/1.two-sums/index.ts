8c321da67f478bc787671d9fe390876f19582a79
EmilyYoung71415 <yxy4work@gmail.com>
2023-12-20 01:29:51 +0800

    feat: 1.two-sum

--- Problems/1.two-sums/index.ts
+++ Problems/1.two-sums/index.ts
@@ -16,7 +16,7 @@ export function twoSum(arr: number[], target: number): number[] | null {
 
 // 暴力法 N^2
 function twoSum2(nums: number[], target: number): number[] | null {
-  let len = nums.length;
+  const len = nums.length;
   for (let i = 0; i < len; i++) {
     for (let j = i + 1; j < len; j++) {
       if (nums[i] + nums[j] === target) {
