59ad3a57fab8cf0bb472e5625c74eb82ec6d1a4b
EmilyYoung71415 <yxy4work@gmail.com>
2023-12-23 17:49:51 +0800

    feat: 455.assign cookies

--- greedy/455.assign-cookies/index.test.ts
+++ greedy/455.assign-cookies/index.test.ts
@@ -1,4 +1,4 @@
-import { findContentChildren } from './index';
+import { findContentChildren, findContentChildren2 } from './index';
 
 describe('findContentChildren', () => {
   it('returns the maximum number of content children', () => {
@@ -7,5 +7,19 @@ describe('findContentChildren', () => {
     expect(findContentChildren([1, 2], [1, 2, 3])).toBe(2);
 
     expect(findContentChildren([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).toBe(5);
+
+    expect(findContentChildren([1, 2, 3], [3])).toBe(1);
+  });
+});
+
+describe('findContentChildren loop chid first', () => {
+  it('returns the maximum number of content children', () => {
+    expect(findContentChildren2([1, 2, 3], [1, 1])).toBe(1);
+
+    expect(findContentChildren2([1, 2], [1, 2, 3])).toBe(2);
+
+    expect(findContentChildren2([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])).toBe(5);
+
+    expect(findContentChildren2([1, 2, 3], [3])).toBe(1);
   });
 });
