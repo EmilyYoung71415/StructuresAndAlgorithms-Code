2555b2b28e3581aeb17915c70d7ca5365851118c
EmilyYoung71415 <yxy4work@gmail.com>
2023-12-23 18:52:00 +0800

    feat. 134.gas-station

---
+++ Problems/134.gas-station/index.test.ts
@@ -0,0 +1,17 @@
+import { canCompleteCircuit } from './index';
+
+describe('canCompleteCircuit should work', () => {
+  it('should return the correct starting index when gas and cost arrays have the same length', () => {
+    expect(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])).toBe(3);
+    expect(canCompleteCircuit([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])).toBe(2);
+    expect(canCompleteCircuit([2, 3, 4], [4, 3, 2])).toBe(1);
+  });
+
+  it('should return -1 when there is no possible starting index', () => {
+    expect(canCompleteCircuit([2, 3, 4], [3, 4, 3])).toBe(-1);
+  });
+
+  it('should return the correct starting index when gas and cost arrays are empty', () => {
+    expect(canCompleteCircuit([], [])).toBe(0);
+  });
+});
