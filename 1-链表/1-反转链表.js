cca0b28c5a89046d75b484c783d26b1c33eaf4e1
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:01 +0800

    day78-反转链表

--- 1-链表/1-反转链表.js
+++ 1-链表/1-反转链表.js
@@ -1,4 +1,5 @@
 /***
+ * leetcode 206
  * 反转单链表
  *  Input: 1->2->3->4->5->NULL
     Output: 5->4->3->2->1->NULL
@@ -23,7 +24,8 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
+// 头插法思路借鉴
+function reverseList1(head) {
     let dummy = new ListNode();
     let curr = dummy;
     while(head){
@@ -35,7 +37,7 @@ var reverseList = function(head) {
     return dummy.next ;
 };
 
-function reverseList(head){
+function reverseList2(head){
     // 链尾
     if(head==null||head.next ==null){
         return head;
@@ -46,4 +48,57 @@ function reverseList(head){
     head.next.next = head;
     head.next = null;// 断开原链接 不然要环
     return newhead;
+}
+
+
+/***
+ * 借用循环链表的思想 使用prev记录
+ * 需要遍历两次
+ * 遍历1、将每个阶段的新next使用prev结点记录，同时更新head，遍历结束后head指向链尾
+ * 遍历2、从尾部开始反向遍历（依靠prev结点），遍历时将结点的next指向prev，同时删去prev结点
+ */
+
+function reverseList(head) {
+    if(head==null||head.next ==null){
+        return head;
+    }
+    // 遍历1
+    let prev = null; // 初始化 head 结点的prev为null
+    while(head) {
+        head.prev = prev;
+        prev = head;
+        head = head.next;
+    }
+    // 此时head指向null 指向链尾的是prev
+    head = prev;
+    let p = head;
+    let pprev = p.prev;
+    // 遍历2 将prev指向的值替换到next
+    while (p && pprev) { // 因为这里要引用p.prev 而 while里又要删去prev属性 所以使用pprev变量
+        delete p.prev;
+        p.next = pprev;
+        p = pprev;
+        pprev = p.prev;
+    } 
+
+    // 头结点处理
+    p.next = null;
+    delete p.prev;
+    return head;
+}
+
+// 优化，依赖局部变量修改链表里结点的指向关系
+function reverseList(head) {
+    if(head==null||head.next ==null){
+        return head;
+    }
+    let prev = null;
+    while (head && head.next) {
+        let next_temp = head.next;
+        head.next = prev;
+        prev = head;
+        head = next_temp;        
+    }
+    head.next = prev;
+    return head;
 }
