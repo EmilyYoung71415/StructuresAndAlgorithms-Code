2d4c49bf1de38d9543063094e4b30b804501475d
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:02 +0800

    单例模式

--- 设计模式/3-单例.js
+++ 设计模式/3-单例.js
@@ -19,7 +19,46 @@
         getName
     }
  }())
-console.log(president.name1);
-console.log(president.data1);
-president.getName();
+// console.log(president.name1);
+// console.log(president.data1);
+// president.getName();
+/*****
+ * 例子：
+ *      网页版qq,点击登录，实例化显示登陆框
+ *      但是 全局只能有一个登陆框。所以 该实例化 必须是单例
+ *      
+ * 单例：
+ *     创建单例
+ *     管理单例
+ * 
+ * 
+ */
 
+
+ // 设置 传入 创建单例的函数 ， 单例负责单例。
+ // 返回一个函数
+ function getSingle(fn){
+    var result;
+    // 如果 result 则return result 
+    // 否则 命令result = fn.apply(this,xxx);
+    return function(){
+        return result||(result = fn.apply(this,arguments));
+    }
+ }
+
+
+ var createLoginLayer = function(){  
+    var div = document.createElement( 'div' ); 
+    div.innerHTML = '我是登录浮窗'; 
+    div.style.display = 'none'; 
+    document.body.appendChild( div ); 
+    return div; 
+}; 
+
+
+var createSingleLoginLayer = getSingle( createLoginLayer ); 
+
+document.getElementById( 'loginBtn' ).onclick = function(){ 
+    var loginLayer = createSingleLoginLayer(); 
+    loginLayer.style.display = 'block'; 
+}; 
