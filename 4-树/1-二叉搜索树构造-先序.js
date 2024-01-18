2d75e333e1fd98f1b0a51bf9eff4dcc5a15ec00d
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:12 +0800

    day110-二叉搜索构造-先序

--- 4-树/1-二叉搜索树构造-先序.js
+++ 4-树/1-二叉搜索树构造-先序.js
@@ -4,27 +4,45 @@
  * 先序遍历 preorder 相匹配的二叉搜索树
  */
 /*************
- * 思路：
+ * 思路1：
  *      先序遍历(根左右) 构造二叉树 => 不唯一
  *      构造二叉搜索： 由于多了 左 < 根 < 右 的条件 所以可以约束唯一
  *      => 转换
  *          先序排序后 可得  中序序列
  *          先序 + 中序 => 遍历得到树
  */
-// 4 / 110
 function bstFromPreorder(preorder) {
-    let inorder = preorder.sort((a,b) => a-b);
-    return buildTree(preorder, inorder);
+    if(!preorder.length) return null
+    let rootVal=preorder.shift()
+    let root = new TreeNode(rootVal)
+    root.left=bstFromPreorder(preorder.filter(item=>item<rootVal))
+    root.right=bstFromPreorder(preorder.filter(item=>item>rootVal))
+    return root;
+};
 
-    function buildTree(preorder,inorder){
-        if(!preorder.length||!inorder.length){
-            return null;
+/****
+ * 思路2:
+ *    先序：根左右
+ *    1、get根节点 
+ *    2、数组里根节点之后第一大的数字，该数字左边是根节点左子树，右边是右子树
+ */
+
+function bstFromPreorder(preorder) {
+    return bstFromPreorderCall(preorder, 0, preorder.length-1);
+
+    function bstFromPreorderCall(preorder, start, end) {
+        if (start > end) return null;
+
+        let index = start+1;
+        for(; index <= end; index++) {
+            if (preorder[index] > preorder[start]) {
+                break;
+            }
         }
-        let root = preorder[0];
-        let node  =  new TreeNode(root);
-        let posi = inorder.indexOf(root);
-        node.left = buildTree(preorder.slice(1,posi+1),inorder.slice(0,posi))
-        node.right = buildTree(preorder.slice(posi+1),inorder.slice(posi+1));
-        return node;
+
+        let root = new TreeNode(preorder[start]);
+        root.left = bstFromPreorderCall(preorder, start+1, index-1);
+        root.right = bstFromPreorderCall(preorder, index, end);
+        return root;
     }
 }
