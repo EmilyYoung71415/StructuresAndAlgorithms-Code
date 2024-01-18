f3201a48a0ca99c994b47d401501235614a927a9
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:12 +0800

    day124-二分查找

--- 查找/0-二分查找.js
+++ 查找/0-二分查找.js
@@ -1,30 +1,45 @@
 /**
  *  @desc 二分查找
- *  基本思想：优先与数组的中间元素比较，如果等于中间元素则直接返回，否则取半递归查找  
+ *  二分查找是基于有序数组的
+ *  基本思想：优先与数组的中间元素比较，如果等于中间元素则直接返回，否则取半递归查找
+ *  相对于顺序查找的O(N)复杂度，二分Logn
  * 
- *  代码思维：
- *          循环不变的条件
- *          边界？
- *          数值中相同值的处理      
+ *  注意点：
+ *      1、边界的判断  start = 0, end = arr.length -1 则  while (start <= end) [start, end]
+ *      2、目标值重复：leetcode 34
  */
 
-function binarySearch(data, target, start, end){  
-    if(start>end) return start;
-    var end = end || data.length - 1,  
-        start = start || 0,  
-        //溢出
-        mIndex = Math.floor((start + end) / 2); 
-        //mIndex = start +Math.floor((end - start)/2);
-        
-    if(target < data[mIndex]){  
-        return binarySearch(data, target, start, mIndex-1);  
-    }else{  
-        return binarySearch(data, target, mIndex+1, end);  
-    }  
-    //没有找到
-    //return false;  
-} 
-//var arr = [-34, 1, 3, 4, 5, 8, 34, 45, 65, 87]; 
-var arr = [34,12,5,123,2,745,32,4,10];   //存在栈溢出
+function binarySearch(arr, target) {
+    let start = 0;
+    let end = arr.length - 1;
 
-console.log(binarySearch(arr,123));
+    while (start <= end) {
+        let mid = (end + start) >> 1;
+        if (arr[mid] === target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+
+function binarySearch(arr, target) {
+    return binarySearchCall(arr, target, 0, arr.length - 1);
+
+    function binarySearchCall(arr, target, start, end) {
+        if (start > end) return -1;
+        let mid = (end + start) >> 1;
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] < target) return binarySearchCall(arr, target, mid + 1, end);
+        return binarySearchCall(arr, target, start, mid - 1);
+    }
+}
+
+console.log(binarySearch([1, 2, 3, 4, 6, 10, 20], 10));
