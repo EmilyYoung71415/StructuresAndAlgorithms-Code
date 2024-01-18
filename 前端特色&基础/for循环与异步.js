06bfc62d5c034bfec09b0777199792cdd91f8851
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:02 +0800

    settimeout

--- 前端特色&基础/for循环与异步.js
+++ 前端特色&基础/for循环与异步.js
@@ -3,15 +3,20 @@
 *
 *     闭包、作用域的考法
 */
- 
+// 0 1 2 3 4 
 for(var i = 0; i < 5; i++) {
     console.log(i);
 }
+
+// 分别隔0 1 2 3 4s 输出5 
 for(var i = 0; i < 5; i++) {
     setTimeout(function() {
         console.log(i);
     }, 1000 * i);
 }
+
+// 立即执行函数 传入 i 分别隔0 12 3 4 输出0 1 23 4 
+// 因为创建了闭包
 for(var i = 0; i < 5; i++) {
     (function(i) {
         setTimeout(function() {
@@ -19,6 +24,9 @@ for(var i = 0; i < 5; i++) {
         }, i * 1000);
     })(i);
 }
+
+// 还是输出 5
+
 for(var i = 0; i < 5; i++) {
     (function() {
         setTimeout(function() {
@@ -26,8 +34,31 @@ for(var i = 0; i < 5; i++) {
         }, i * 1000);
     })(i);
 }
+
+// 回调函数传入 参数
 for(var i = 0; i < 5; i++) {
     setTimeout((function(i) {
         console.log(i);
     })(i), i * 1000);
 }
+
+// settimeout 可以传入第三个参数 一旦定时器到期，作为参数传递给function
+for(var i=1;i<3;i++){
+    setTimeout((i)=>{
+        console.log(i)
+    },i*1000,i)
+}
+/*
+function foo(){
+    var arr = [];
+    for(var i = 0; i < 2; i++){
+        // 并没有把函数的返回值赋值给数组元素，而仅仅是把函数赋值给了数组元素
+        arr[i] = (function(j){
+            return j;
+        })(i)
+    }
+    return arr;
+}
+var bar = foo();
+console.log(bar[0]());//2 
+*/
