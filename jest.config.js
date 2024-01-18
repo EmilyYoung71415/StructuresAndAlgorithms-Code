ca4158e26881dfe252d2258ffbac5e55654f4ad5
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:13 +0800

    chore: ts & jest & prettier support

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
