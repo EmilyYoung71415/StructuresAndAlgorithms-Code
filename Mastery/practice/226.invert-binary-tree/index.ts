eaf67b465a03f1da20d1d726eee0c05a2e1d1672
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:15 +0800

    feat: 226 invertTree

---
+++ Mastery/practice/226.invert-binary-tree/index.ts
@@ -0,0 +1,11 @@
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) return null;
+
+  invertTree(root.left);
+  invertTree(root.right);
+  const nodeL = root.left;
+  const nodeR = root.right;
+  root.left = nodeR;
+  root.right = nodeL;
+  return root;
+}
