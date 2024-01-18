69429fe4eb21632520bd9264f0fe1ece48055116
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:02 +0800

    链表整洁

--- 1-链表/1-两个有序链表合并.js
+++ 1-链表/1-两个有序链表合并.js
@@ -1,4 +1,5 @@
 /****
+ * leetcode 21
  * Merge two sorted linked lists and return it as a new list
  * 
  *  Input: 1->2->4, 1->3->4
@@ -34,26 +35,6 @@ var mergeTwoLists = function(l1, l2) {
 };
 
 // 迭代
-//===> 结果是 返回了合并之后的链尾
-// 实际是改变的一直是头结点？
-function mergeTwoLists(l1,l2){
-    let list = new ListNode();
-    while(l1&&l2){
-        if(l1.val<l2.val){
-            list.next = l1;
-            l1 = l1.next;
-        }else{
-            list.next = l2;
-            l2 = l2.next;
-        }
-        list = list.next
-    }
-
-    list = l1?l1:l2;
-    return list
-}
-
-
 function mergeTwoLists(l1,l2){
     let dummy = new ListNode();// 头结点
     let curr = dummy;
@@ -68,7 +49,6 @@ function mergeTwoLists(l1,l2){
         curr = curr.next;
     }
 
-    //curr = l1?l1:l2;
     curr.next = l1?l1:l2
     return dummy.next;
 }
