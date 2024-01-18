9978d1d3d05ed63e6a76bf793bf58cf1651619c0
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:03 +0800

    day95-层序构建满二叉

--- 4-树/1-遍历序列构建满二叉树.js
+++ 4-树/1-遍历序列构建满二叉树.js
@@ -9,7 +9,7 @@
     中序：[#,2,#,1,#,4,#,3,#,5,#]
     后序: [#,#,2,#,#,4,#,#,5,3,1]
 */
-console.log(arrToTree('1,2,#,#,3,4,#,#,5,#,#'.split(',')));
+// console.log(arrToTree('1,2,#,#,3,4,#,#,5,#,#'.split(',')));
 
 function TreeNode(val) {
     this.val = val;
@@ -17,7 +17,7 @@ function TreeNode(val) {
 }
 
 // 前序转树
-function arrToTree1(arr) {
+function arrToTree(arr) {
     let nodeval = arr.shift();
     if (nodeval === '#' || !nodeval) return null;
 
@@ -30,3 +30,24 @@ function arrToTree1(arr) {
 // 后序转树 后序：左右根 reverse 调用前序
 
 // 中序转树？
+
+
+// 层序转树 [1,2,3,4,5] 2*i+1 是左子节点 2*i+2是右子节点
+/***
+ * 从当前节点 顺势链接子节点 =》 不可行
+ * 从当前节点 找到父节点 父节点链接当前节点
+ */
+function arrToTreeLevel(arr) {
+    // nodeArr 从1开始计数
+    let nodeArr = arr.map(val=>{
+        return val === '#' ? null : new TreeNode(val);
+    });
+
+    for(let i = 1; i<nodeArr.length; i++) {
+        let fIndex = Math.floor((i-1)/2);
+        let father = nodeArr[fIndex];
+        i % 2 ? father.left = nodeArr[i] : father.right = nodeArr[i];
+    }
+
+    return nodeArr[0];
+}
