01b2e0f73faf218efbf4629248fe4100cded0bc6
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:02 +0800

    day84-有序数组&有序链表的去重

--- 0-数组/1-有序数组去重.js
+++ 0-数组/1-有序数组去重.js
@@ -8,19 +8,45 @@
  * 需要两个指针，i指向不含重复元素的数组末尾，j为原数组遍历元素
  *                                                                                                                            
  */
-unionSet([1,1,2]);
+unionSet([1,2,2,4]);
 function unionSet(arr) {
     if (!arr || !arr.length) return;
 
-    for (let i=0,j=1,len=arr.length;j<len;j++) {
-        if (arr[j] !== arr[i]) {
-            arr[++i] = arr[j];
+    // for (let i=0,j=1,len=arr.length;j<len;j++) {
+    //     if (arr[j] !== arr[i]) {
+    //         arr[++i] = arr[j];
+    //     }
+    //     if (j===len-1) {
+    //         arr.length = i+1;
+    //     }
+    // }
+    let slow = 0;
+    let fast = 1;
+    while (fast < arr.length) {
+        if(arr[slow] !== arr[fast]) {
+            arr[++slow] = arr[fast];
         }
-        if (j===len-1) {
-            arr.length = i+1;
-        }
+        fast++;
     }
-    console.log(arr);
+    arr.length = slow+1;
 }
 
 // 扩展： 如果是无序数组，时间复杂度为O(N)要求，求解决方案？ ==> hash表
+
+// 扩展：有序链表去重
+function deleteDulplicates(head) {
+    if (head==null) return;
+
+    let slow = head;
+    let fast = head.next;
+
+    while (fast) {
+        if (fast.val !== slow.val) {
+            // arr[++slow] = arr[fast]
+            slow.next = fast;
+            slow = slow.next;
+        }
+        fast = fast.next;
+    }
+    slow.next = null;
+}
