0a60f963d4d7a051d1a65fb3a8b3b67919a7a23e
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:17 +0800

    perf: cleaner code

--- Problems/572.subtree-of-another-tree/index.ts
+++ Problems/572.subtree-of-another-tree/index.ts
@@ -1,3 +1,5 @@
+// 时间复杂度: O(n * m)
+// n: root 节点数；m: subRoot 节点数
 export function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
   if (subRoot === null) return true;
   if (root === null) return false;
@@ -5,6 +7,26 @@ export function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): bool
   if (root.val === subRoot.val && isSameTree(root, subRoot)) return true;
 
   return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+  function isSameTree(root: TreeNode | null, subRoot: TreeNode | null) {
+    if (root === null && subRoot === null) return true;
+
+    if (root && subRoot && root.val === subRoot.val) {
+      return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+    }
+
+    return false;
+  }
+}
+
+// 代码优化版
+export function isSubtree2(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (subRoot === null) return true;
+  if (root === null) return false;
+
+  return (
+    isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+  );
 
   function isSameTree(root: TreeNode | null, subRoot: TreeNode | null) {
     if (root === null && subRoot === null) return true;
