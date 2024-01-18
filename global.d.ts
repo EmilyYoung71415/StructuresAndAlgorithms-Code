837804c3713ae386fafa1921b923c8a5419feb85
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:13 +0800

    feat: d.ts

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare interface ListNode {
+  value: number; // 可以换为T
+  next: ListNode | null;
+}
+
+declare interface TreeNode {
+  value: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+}
