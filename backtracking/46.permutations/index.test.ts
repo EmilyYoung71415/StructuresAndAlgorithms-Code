7258eb82ab22dee6d515da85ad966f8ab885d807
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:15 +0800

    feat: 46.permutations

---
+++ backtracking/46.permutations/index.test.ts
@@ -0,0 +1,31 @@
+import { serializeArr } from '@utils';
+import { permute } from './index';
+
+test('permute should work1', () => {
+  const output = permute([1, 2, 3]);
+  expect(serializeArr(output)).toBe(
+    serializeArr([
+      [1, 2, 3],
+      [1, 3, 2],
+      [2, 1, 3],
+      [2, 3, 1],
+      [3, 1, 2],
+      [3, 2, 1],
+    ]),
+  );
+});
+
+test('permute should work2', () => {
+  const output = permute([0, 1]);
+  expect(serializeArr(output)).toBe(
+    serializeArr([
+      [0, 1],
+      [1, 0],
+    ]),
+  );
+});
+
+test('permute should work3', () => {
+  const output = permute([1]);
+  expect(serializeArr(output)).toBe(serializeArr([[1]]));
+});
