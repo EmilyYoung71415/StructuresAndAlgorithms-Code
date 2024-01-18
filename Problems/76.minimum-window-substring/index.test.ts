0fb5660534caeb39d4759e13b758056ec0414bbc
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:17 +0800

    feat.76.minWindow

--- Problems/76.minimum-window-substring/index.test.ts
+++ Problems/76.minimum-window-substring/index.test.ts
@@ -1,4 +1,4 @@
-import { minWindow } from './index';
+import { minWindow, minWindow_perf } from './index';
 
 describe('239.minimum-window-substring', () => {
   it('case 1', () => {
@@ -11,5 +11,23 @@ describe('239.minimum-window-substring', () => {
 
   it('case 3', () => {
     expect(minWindow('a', 'aa')).toBe('');
+  });
+});
+
+describe('239.minimum-window-substring minWindow_perf', () => {
+  it('case 1', () => {
+    expect(minWindow_perf('ADOBECODEBANC', 'ABC')).toBe('BANC');
+  });
+
+  it('case 2', () => {
+    expect(minWindow_perf('a', 'a')).toBe('a');
+  });
+
+  it('case 3', () => {
+    expect(minWindow_perf('a', 'aa')).toBe('');
+  });
+
+  it('case 4', () => {
+    expect(minWindow_perf('ab', 'a')).toBe('a');
   });
 });
