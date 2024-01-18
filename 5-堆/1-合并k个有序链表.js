6aba8e6b94a0d6f8a2cdef5300dfce58c9a2358b
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:12 +0800

    day117-合并k个有序链表 多解

--- 5-堆/1-合并k个有序链表.js
+++ 5-堆/1-合并k个有序链表.js
@@ -17,61 +17,18 @@
  *      多个：
  *          1、多个迭代指针，找出最小的那个
  *          ===》 实时数据流里找出最小的那个数 典型的堆问题
- *      时间复杂度：Nlogk，空间：O(n) + O(k)
+ *      时间复杂度：Nlogk，空间：O(k)
  * 
- *      way2: 分治
+ *      way2: 逐一两两合并链表 时间：O(kn) 空间O(1)
+ *      way3: 分治 Nlogk
+ *          第一轮合并以后，k个链表被合并成了 k/2 个链表，然后是k/4,k/8... 直至合并成一个链表
+ *          k次的合并 被简化成了 logk次
  */
 // way1: 
-class Heap {
-    constructor(comparator) {
-        this.data = [];
-        this.comparator = comparator || ((parent, child) => parent - child);
-    }
-
-    get size() {
-        return this.data.length;
-    }
-
-    bubbleUp(c) {
-        if (c === 0) return;
-        const p = Math.floor((c + 1) / 2) - 1;
-        if (this.comparator(this.data[p], this.data[c]) > 0) {
-            [this.data[p], this.data[c]] = [this.data[c], this.data[p]];
-        }
-        this.bubbleUp(p);
-    }
-
-    bubbleDown(p) {
-        const c = 2 * (p + 1) - 1;
-        if (c >= this.data.length) return;
-
-        const leftDelta = this.comparator(this.data[p], this.data[c]);
-        const rightDelta = c + 1 >= this.data.length ? 0 : this.comparator(this.data[p], this.data[c + 1]);
-        if (leftDelta <= 0 && rightDelta <= 0) return;
-
-        const swapChildIndex = c + (leftDelta <= rightDelta);
-        [this.data[p], this.data[swapChildIndex]] = [this.data[swapChildIndex], this.data[p]];
-        this.bubbleDown(swapChildIndex);
-    }
-
-    add(val) {
-        this.data.push(val);
-        this.bubbleUp(this.data.length - 1);
-    }
-
-    poll() {
-        if (this.size < 2) return this.data.pop();
-        [this.data[0], this.data[this.size - 1]] = [this.data[this.size - 1], this.data[0]];
-        const val = this.data.pop();
-        this.bubbleDown(0);
-        return val;
-    }
-}
-
 function mergeKLists(lists) {
     if (!lists.length) return null;
     
-    const minHeap = new Heap((parent, child) => parent.val - child.val);
+    const minHeap = new Heap('min');
     // 将链表的头节点放入堆里 此后的遍历 根据各自的头节点找next节点 
     for (let node of lists) {
         node && minHeap.add(node);
@@ -89,8 +46,28 @@ function mergeKLists(lists) {
     return dummy.next;
 }
 
+// way2
+function mergeKLists(lists) {
+    let linkedList = null;
+    for (let newlist of lists) {
+        linkedList = mergeTwoLists(linkedList, newlist);
+    }
+    return linkedList;
 
-// way2 
+    function mergeTwoLists(l1, l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val<l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next  = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    };
+};
+
+// way3 
 function mergeKLists(lists) {
     return mergeKListsCall(lists, 0, lists.length - 1);
 
