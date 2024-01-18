cfea29a7dbb2b6b26417c4b004ea5114e4adc183
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:12 +0800

    day103-二叉树最近公共祖先x2

---
+++ z-剑指offer/2-基础数据结构-树-链表-队列-栈/68-二叉树最近公共祖先.js
@@ -0,0 +1,61 @@
+/***
+ * https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+ * 
+ */
+
+/*****************
+ * 思路：
+ *      后序 左右根 迭代时 此时的栈全是祖先元素
+ */
+function lowestCommonAncestor(root,k,q){
+    let stack = [],
+        p = root,
+        stack1 = [],
+        firstMeetNode = null,
+        prev = null;
+    while(p||stack.length>0){
+        if(p){
+            stack.push(p)
+            p = p.left;
+        }
+        else{
+            let topNode = stack[stack.length-1];
+            if(topNode.right&&topNode.right!=prev){
+                p = topNode.right;
+                stack.push(p)
+                p = p.left
+            }
+            else{
+                let node = topNode; 
+                if(firstMeetNode==null&&(node==q||node==k)){
+                    firstMeetNode = node;
+                     stack1 = [...stack];
+                }
+                // 将两个栈一一比对  确定node遇见了第二个节点
+                if((node==q||node==k)&&node!=firstMeetNode){
+                    for(let i=stack1.length-1;i>0;i--){
+                        for(let j=stack.length-1;j>0;j--){
+                            if(stack1[i]==stack[j]){
+                                return stack1[i];
+                            }
+                        }
+                    }
+                }
+                prev = node;
+                p = null;
+                stack.pop()
+            }
+        }
+    }
+    return root;
+}
+
+/***
+ *  递归 
+ */
+function lowestCommonAncestor(root,q,p){
+    if(root==null || root==q || root==p) return root;
+    let left = lowestCommonAncestor(root.left,q,p)
+    let right = lowestCommonAncestor(root.right,q,p)
+    return (left && right) ? root: (left || right);
+}
---
+++ z-剑指offer/2-基础数据结构-树-链表-队列-栈/68-二叉树最近公共祖先1-二叉搜索树.js
@@ -0,0 +1,30 @@
+/***
+ * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+ */
+// 和二叉树的最近公共祖先对比， 二叉搜索树的性质能给题解带来什么便利
+// 1、若两个结点在位于某结点两边，那么某结点即为所求
+function lowestCommonAncestor(root, p, q) {
+    let res = null;
+
+    while (root) {
+        if (root.val > p.val && root.val > q.val) {
+            root = root.left;
+        }
+        else if (root.val < p.val && root.val < q.val) {
+            root = root.right;
+        }
+        else {
+            res = root;
+            break;
+        }
+    }
+    return res;
+}
+
+
+// 递归版
+function lowestCommonAncestor(root, p, q) {
+    if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+    if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+    return root;
+}
