68a0f4f0868da2118a383f50f4d60dfe26154ce5
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:49:36 +0800

    26.Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var j = 0;
+    for(var i=1;i<nums.length;i++){
+    	if(nums[i]!=nums[j]){
+    		j++;
+    		if(i!=j){
+    			nums[j]=nums[i];
+    		}
+    	}
+    }
+    return j+1;
+    
+}
