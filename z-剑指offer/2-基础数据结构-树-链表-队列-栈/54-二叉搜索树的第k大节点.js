d55bd67842dc883b4e93daef287375214312ccd6
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:12 +0800

    day104-二叉搜索树的第k大节点

---
+++ z-剑指offer/2-基础数据结构-树-链表-队列-栈/54-二叉搜索树的第k大节点.js
@@ -0,0 +1,20 @@
+/****
+ * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+ */
+
+/***
+ * 二叉搜索的 中序遍历是升序输出
+ * 
+ */
+function kthLargest(root, k) {
+    let result = [];
+    kthLargestCall(root);
+    return result[result.length-k];
+
+    function kthLargestCall(root) {
+        if (!root) return;
+        root.left && kthLargestCall(root.left);
+        result.push(root.val);
+        root.right && kthLargestCall(root.right);
+    }
+}
