4e885d138e5a6ea15e5a54a0894c7eb5fefa4d49
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-18 22:50:16 +0800

    feat. 134.gas-station greedy

--- Problems/134.gas-station/index.test.ts
+++ Problems/134.gas-station/index.test.ts
@@ -1,4 +1,5 @@
 import { canCompleteCircuit } from './index';
+import { canCompleteCircuit as canCompleteCircuit_greedy } from './index';
 
 describe('canCompleteCircuit should work', () => {
   it('should return the correct starting index when gas and cost arrays have the same length', () => {
@@ -13,5 +14,21 @@ describe('canCompleteCircuit should work', () => {
 
   it('should return the correct starting index when gas and cost arrays are empty', () => {
     expect(canCompleteCircuit([], [])).toBe(0);
+  });
+});
+
+describe('canCompleteCircuit should work', () => {
+  it('should return the correct starting index when gas and cost arrays have the same length', () => {
+    expect(canCompleteCircuit_greedy([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])).toBe(3);
+    expect(canCompleteCircuit_greedy([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])).toBe(2);
+    expect(canCompleteCircuit_greedy([2, 3, 4], [4, 3, 2])).toBe(1);
+  });
+
+  it('should return -1 when there is no possible starting index', () => {
+    expect(canCompleteCircuit_greedy([2, 3, 4], [3, 4, 3])).toBe(-1);
+  });
+
+  it('should return the correct starting index when gas and cost arrays are empty', () => {
+    expect(canCompleteCircuit_greedy([], [])).toBe(0);
   });
 });
