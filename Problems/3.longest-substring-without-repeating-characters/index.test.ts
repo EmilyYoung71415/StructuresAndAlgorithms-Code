3ead43cbc9f0faf14716f5f8574fac240e35287c
EmilyYoung71415 <yxy4work@gmail.com>
2024-01-21 11:30:34 +0800

    refactor: using jest each to simplify test code

--- Problems/3.longest-substring-without-repeating-characters/index.test.ts
+++ Problems/3.longest-substring-without-repeating-characters/index.test.ts
@@ -1,55 +1,21 @@
 import { lengthOfLongestSubstring } from './index';
 import { lengthOfLongestSubstring as lengthOfLongestSubstring2 } from './2';
 
+const testCases = [
+  { s: 'abcabcbb', expected: 3 },
+  { s: 'bbbbb', expected: 1 },
+  { s: 'pwwkew', expected: 3 },
+  { s: 'dvdf', expected: 3 },
+];
+
 describe('lengthOfLongestSubstring should work', () => {
-  it('use case 1', () => {
-    const s = 'abcabcbb';
-    const expected = 3;
+  test.each(testCases)('use case %#', ({ s, expected }) => {
     expect(lengthOfLongestSubstring(s)).toBe(expected);
-  });
-
-  it('use case 2', () => {
-    const s = 'bbbbb';
-    const expected = 1;
-    expect(lengthOfLongestSubstring(s)).toBe(expected);
-  });
-
-  it('use case 3', () => {
-    const s = 'pwwkew';
-    const expected = 3;
-    expect(lengthOfLongestSubstring(s)).toBe(expected);
-  });
-
-  // 重复元素在队首
-  it('use case 4', () => {
-    const s = 'dvdf';
-    const expected = 3;
-    expect(lengthOfLongestSubstring2(s)).toBe(expected);
   });
 });
 
 describe('lengthOfLongestSubstring2 should work', () => {
-  it('use case 1', () => {
-    const s = 'abcabcbb';
-    const expected = 3;
-    expect(lengthOfLongestSubstring2(s)).toBe(expected);
-  });
-
-  it('use case 2', () => {
-    const s = 'bbbbb';
-    const expected = 1;
-    expect(lengthOfLongestSubstring2(s)).toBe(expected);
-  });
-
-  it('use case 3', () => {
-    const s = 'pwwkew';
-    const expected = 3;
-    expect(lengthOfLongestSubstring2(s)).toBe(expected);
-  });
-
-  it('use case 4', () => {
-    const s = 'dvdf';
-    const expected = 3;
+  test.each(testCases)('use case %#', ({ s, expected }) => {
     expect(lengthOfLongestSubstring2(s)).toBe(expected);
   });
 });
